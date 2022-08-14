9efe55acdaf6c7dd77fa11f188e8b99fe3daef1c
unknown <nugraha.fajrian@gmail.com>
2022-08-15 00:50:14 +0700

    first commit

---
+++ src/main.js
@@ -0,0 +1,77 @@
+import express from "express";
+import { PrismaClient } from "@prisma/client";
+
+const database = new PrismaClient();
+
+const app = express();
+
+app.use(express.json());
+
+const port = 7000;
+
+app.get("/mahasiswa", async (req, res) => {
+  try {
+    const mahasiswa = await database.mahasiswa.findMany();
+    if (!mahasiswa) throw new Error("Mahasiswa gak ada");
+    res.send(mahasiswa);
+  } catch (err) {
+    res.send({ status: 404, message: err.message });
+  }
+});
+
+app.get("/mahasiswa/:id", async (req, res) => {
+  try {
+    const mahasiswa = await database.mahasiswa.findUnique({
+      where: {
+        id: parseInt(req.params.id),
+      },
+    });
+    if (!mahasiswa) throw new Error("mahasiswa gak ada");
+
+    res.send(mahasiswa);
+  } catch (err) {
+    res.send({ status: 404, message: err.message });
+  }
+});
+
+app.post("/mahasiswa/create", async (req, res) => {
+  try {
+    const mahasiswa = await database.mahasiswa.create({
+      data: {
+        name: req.body.name,
+        email: req.body.email,
+        nim: req.body.nim,
+      },
+    });
+    res.send({ message: "Mahasiswa Berhasil di buat", data: mahasiswa });
+  } catch (err) {}
+});
+
+app.put("/mahasiswa/update/", async (req, res) => {
+  try {
+    const mahasiswa = await database.mahasiswa.update({
+      where: {
+        id: req.body.id,
+      },
+      data: {
+        name: req.body.name,
+        email: req.body.email,
+        nim: req.body.nim,
+      },
+    });
+    res.send({ message: "Mahasiswa Berhasil di update", data: mahasiswa });
+  } catch (err) {}
+});
+
+app.delete("/mahasiswa/delete", async (req, res) => {
+  await database.mahasiswa.delete({
+    where: {
+      id: req.body.id,
+    },
+  });
+  res.send({ message: "Mahasiswa Berhasil di hapus" });
+});
+
+app.listen(port, () => {
+  console.log(`Aplikasi nya jalan di port ${port}`);
+});
